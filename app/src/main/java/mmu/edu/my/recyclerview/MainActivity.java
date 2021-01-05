10b512c820d3cc150b72dcea4d89bf867ceb04ad
febbyfitri <febbyftrp@student.telkomuniversity.ac.id>
2021-01-05 22:00:55 +0700

    tutorial 5

--- app/src/main/java/mmu/edu/my/recyclerview/MainActivity.java
+++ app/src/main/java/mmu/edu/my/recyclerview/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.LruCache;
 import android.view.View;
@@ -58,36 +61,66 @@ public class MainActivity extends AppCompatActivity {
         MyAdapter myAdapter = new MyAdapter();
         //myAdapter.addElements(versions);
         recyclerView.setAdapter(myAdapter);
-        queue = Volley.newRequestQueue(this);
-            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, " https://raw.githubusercontent.com/kenobicjj/android/main/tutorial4.json ",
-            null,
-                    new Response.Listener<JSONArray>() {
-                        @Override
-                        public void onResponse(JSONArray response) {
-                            versions.clear();
-                            for(int i = 0;i < response.length();i++) {
-                                try {
-                                    JSONObject item = response.getJSONObject(i);
-                                    Toast.makeText(MainActivity.this, item.toString(), Toast.LENGTH_SHORT).show();
-                                    String name = item.getString("name");
-                                    String description = item.getString("description");
-                                    String icon = item.getString("icon");
-                                    Version version =new Version(name, description, icon);
-                                    versions.add(version);
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                            myAdapter.addElements(versions);
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
 
-                        }
-                    });
-            queue.add(request);
+            Cursor cursor = getContentResolver().query(
+                    Uri.parse("content://mmu.edu.my.recyclerview.provider"),
+                    new String[]{"name","description","icon"}, null, null, "name");
+
+            queue = Volley.newRequestQueue(this);
+            if(cursor.getCount() == 0) {
+                Toast.makeText(MainActivity.this, "Load from Repo", Toast.LENGTH_SHORT).show();
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, " https://raw.githubusercontent.com/kenobicjj/android/main/tutorial4.json ",
+                        null,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                versions.clear();
+                                for(int i = 0;i < response.length();i++) {
+                                    try {
+                                        JSONObject item = response.getJSONObject(i);
+                                        Toast.makeText(MainActivity.this, item.toString(), Toast.LENGTH_SHORT).show();
+                                        String name = item.getString("name");
+                                        String description = item.getString("description");
+                                        String icon = item.getString("icon");
+                                        Version version =new Version(name, description, icon);
+                                        versions.add(version);
+                                        ContentValues values = new ContentValues(3);
+                                        values.put("name", name);
+                                        values.put("description", description);
+                                        values.put("icon", icon);
+                                        getContentResolver().insert(Uri.parse("content://mmu.edu.my.recyclerview.provider"), values);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                myAdapter.addElements(versions);
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+                queue.add(request);
+            } else {
+                cursor.moveToFirst();
+                while(!cursor.isAfterLast()) {
+
+                    String name = cursor.getString(0);
+                    String description = cursor.getString(1);
+                    String icon = cursor.getString(2);
+                    Version version = new Version(name, description, icon);
+                    versions.add(version);
+                    cursor.moveToNext();
+
+                }
+                cursor.close();
+                Toast.makeText(MainActivity.this, "Load from database", Toast.LENGTH_SHORT).show();
+                myAdapter.addElements(versions);
+            }
+
+
     }
 
     class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
